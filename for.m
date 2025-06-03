ab847878aa26cee65e83cf5b2a4896b075c04ddf
Dinuka_silva <dinukaavindra@gmail.com>
2025-06-03 15:34:52 +0530

    Update for.m

--- for.m
+++ for.m
@@ -6,5 +6,19 @@ for variable1=1:2:15
     disp(variable1)
 end
 
+%write a for loop to calculate the sum of the numbers from 1 to 10
 
+sum=0;
+for i=1:10
+    sum=sum+i;
+end
+disp("The sum is: "+sum)
+
+%write a for loop to calculate the sum of the numbers from 1 to 10
+
+sum=0;
+for i=1:10
+    sum=sum+i;
+end
+disp("The sum is: "+sum)
 
